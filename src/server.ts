2456cf1fd8a69979fcdba1f89b440410aa0c4ea8
Fernando Falci <fernando.falci@adevinta.com>
2022-03-22 08:05:42 +0100

    revert

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,7 @@
 import express from 'express';
-import https from 'https';
-import { DANEAgent, setServers, lookup } from 'https-dane';
+import hip2 from 'hip2-dane';
 
-const agent = new DANEAgent();
-type FN = (data: any) => void;
-
-const get = (url: string, resolve: FN, reject: FN, redirect: number = 0) => {
-  if (redirect > 5) {
-    reject(new Error('Too many redirects'));
-    return;
-  }
-  const req = https.get(url, { agent, lookup }, (res) => {
-    if (res.statusCode === 301 || res.statusCode === 302) {
-      return get(res.headers.location!, resolve, reject, redirect + 1);
-    }
-
-    let data = '';
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => (data += chunk));
-    res.on('end', () => {
-      if (res.statusCode! >= 400) {
-        const error = new Error(res.statusMessage);
-        (error as any).code = res.statusCode;
-
-        return reject(error);
-      }
-      resolve(data.trim());
-    });
-    req.on('error', (error) => reject(error));
-    req.end();
-  });
-};
-
-function fetchAddress(name: string, token: string = 'HNS') {
-  return new Promise((resolve, reject) => {
-    const url = `https://${name}/.well-known/wallets/${token.toUpperCase()}`;
-
-    get(url, resolve, reject);
-  });
-}
-
-setServers([process.env.DNS!]);
+hip2.setServers([process.env.DNS!]);
 
 const app = express();
 
@@ -49,7 +10,8 @@ app.get('/', (req, res) => {
   const token = (req.query.token as string) || 'HNS';
   res.header('Access-Control-Allow-Origin', '*');
 
-  fetchAddress(name, token)
+  hip2
+    .fetchAddress(name, token)
     .then((addr) => {
       res.json({ addr });
     })
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,3 +8,10 @@ declare module 'https-dane' {
   export function setServers(servers: string[]): void;
   export const lookup: LookupFunction;
 }
+declare module 'hip2-dane' {
+  const hip2: {
+    fetchAddress(name: string, token: string): Promise<string>;
+    setServers(servers: string[]): void;
+  };
+  export default hip2;
+}
